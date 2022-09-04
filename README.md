bb194b8adbf73324f54a58b56b9f646fbcb62722
Henrychang26 <henrychang26@gmail.com>
2022-09-03 22:55:06 -0700

    Dynamic SVG NFT

--- README.md
+++ README.md
@@ -2,4 +2,11 @@ creating 3 different contracts
 
 1. Basic NFT
 2. Random IPFS NFT
+-pros: cheap
+-cons: someone needs to pin our data
 3. Dynamic SVG NFT
+-pros: The data is on chain
+-cons: much more expense
+
+//if price of ETH is above X -> Happy Face
+//if price of ETH is below x -> frowny face
---
+++ contracts/DynamicSvgNft.sol
@@ -0,0 +1,32 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.7;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "base64-sol/base64.sol";
+
+contract DynamicSvgNft is ERC721 {
+    //Mint
+    //store the SVG somewhere
+    //some logic to say "Show X image" or "show Y Image"
+
+    uint256 private s_tokenCounter;
+    string private i_lowImageURI;
+    string private i_highImageURI;
+    string private constant base64EncodedSvgPrefix = "data:image/svg+xnl;base64,";
+
+    constructor(string memory lowSvg, string memory highSvg) ERC721("Dynamic SVG NFT", "DSN") {
+        s_tokenCounter = 0;
+    }
+
+    function svgToImageURI(string memory svg) public pure returns (string memory) {
+        //This function will spit out URL or URI
+        string memory svgBase64Encoded = Base64.encode(bytes(string(abi.encodePacked(svg))));
+        return string(abi.encodePacked(base64EncodedSvgPrefix, svgBase64Encoded));
+    }
+
+    function mintNft() public {
+        _safeMint(msg.sender, s_tokenCounter); //check docs for params
+        s_tokenCounter = s_tokenCounter + 1;
+    }
+}
